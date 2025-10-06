51ba4e1c82e25b08495f9ed668d358aa3bd0eb18
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-10-07 01:52:57 +0500

    shell scripting init

---
+++ first-shell-script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "hello shaka-g ki hal a"
---
+++ sample-shell-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create a sample folder
+
+mkdir sample-folder
+
+# create two files file in sample folder
+cd sample-folder
+touch firstfile.txt secondfile.txt
