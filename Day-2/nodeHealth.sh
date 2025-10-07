652f6efa5ca6edd1d7663ab94d6c3f489733c055
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-10-07 17:10:07 +0500

    edited

--- Day-2/nodeHealth.sh
+++ Day-2/nodeHealth.sh
@@ -9,8 +9,20 @@
 # Version: v1
 #############################
 
-set -x # debug mode
-# set -x will print the being excuted command
+# set -x # debug mode
+# set -x will print the being executed command
+# set -e # exit the script when there is an error
+# set -o pipefail
+
+set -xeo pipefail
+
+
+# asdfjalaskja # Due to set -e when the command is not found the execution of the script stops
+# But if we only use it alone and if we use a command like that:
+# ashadadafd | echo # set -e does n't stop the script here it say if we are using pipe 
+# and the last command of pipe works fine then i ll will not stop my execution.
+# To prevent that we will use the set -o 
+asdafdadfa | echo "Hello"
 
 echo "Print the disk space"
 df -h
@@ -20,3 +32,5 @@ free -g
 
 echo "Print the number of cpus"
 nproc
+
+ps -ef | grep vlc | awk -F" " '{print $2}'
