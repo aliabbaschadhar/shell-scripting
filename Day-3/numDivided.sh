e517039f4c9e7a9678a6aa3cced557db86146d48
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-10-07 21:57:37 +0500

    There is no such number exists

--- Day-3/numDivided.sh
+++ Day-3/numDivided.sh
@@ -3,7 +3,7 @@
 # Write a shell script to print the numbers divided by 3 & 5 and not divided 15
 
 for i in {1..100};do
-	if ((( $i%3==0 && $i%5==0 ) && ( $i%15!=0 ) ));then
+	if (( $i%3==0 && $i%5==0 && $i%15!=0 ));then
 		echo "Number is : $i "
 	fi
 done
